25400a51b4ae700f0d9dbf290c416bd264b7b30f
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2022-10-27 19:51:35 +0530

    gate

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { persistor } from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
